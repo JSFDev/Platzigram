d520b876f79af85bcb7f95a5edf8801c7e809022
Salva Crespo <cresponline@gmail.com>
2016-07-09 17:13:16 +0200

    gulp tasks - styles

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename');
+
+gulp.task('styles', function() {
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles'])
